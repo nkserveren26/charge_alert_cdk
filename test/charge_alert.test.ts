3e90a47962d62a1e89f432dc213c9ea7719a3049
nkserveren26 <norinoritoshi@outlook.jp>
2023-07-29 10:25:04 +0900

    Initial commit

---
+++ test/charge_alert.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ChargeAlert from '../lib/charge_alert-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/charge_alert-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ChargeAlert.ChargeAlertStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
