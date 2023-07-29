57e0b3876e59d0a1230ae31215b109da462ce2ea
nkserveren26 <norinoritoshi@outlook.jp>
2023-07-29 11:04:34 +0900

    commit

--- lib/services/budgets/creator.ts
+++ lib/services/budgets/creator.ts
@@ -1,3 +1,17 @@
+import { CfnBudget } from "aws-cdk-lib/aws-budgets";
+import { Construct } from "constructs";
+
 export class BudgetsCreator {
-    
+    public static createBudgets(self: Construct, budgetsName: string) {
+        const budgets = new CfnBudget(self, budgetsName, {
+            budget: {
+                budgetType: "COST",
+                timeUnit: "MONTHLY",
+                budgetLimit: {
+                    amount: 80,
+                    unit: 'USD',
+                },
+            },
+        });
+    }
 }
