34c37fc7f2b30ea7ac49b5c8759603a115cc42d8
nkserveren26 <norinoritoshi@outlook.jp>
2023-08-05 16:36:58 +0900

    commit

---
+++ lib/services/cost_anomaly_detection/creator.ts
@@ -0,0 +1,17 @@
+import { CfnAnomalyMonitor } from "aws-cdk-lib/aws-ce";
+import { Construct } from "constructs";
+
+export class CostAnomalyDetectionCreator {
+    public static createCostMonitorForAWSServices(
+        self: Construct, 
+        monitorName: string,
+        ): CfnAnomalyMonitor {
+            const costMonitor = new CfnAnomalyMonitor(self, monitorName, {
+                monitorName: monitorName,
+                monitorType: "DIMENSIONAL",
+                monitorDimension: "SERVICE",
+            });
+            return costMonitor;
+        }
+
+}
