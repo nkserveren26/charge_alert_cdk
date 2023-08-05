f2c612cd3f49d03347d4a4e2946ea5f43ac28059
nkserveren26 <norinoritoshi@outlook.jp>
2023-08-05 16:01:45 +0900

    commit

---
+++ lib/services/iam/creator.ts
@@ -0,0 +1,30 @@
+import { Effect, PolicyStatement, Role, ServicePrincipal } from "aws-cdk-lib/aws-iam";
+import { Topic } from "aws-cdk-lib/aws-sns";
+import { Construct } from "constructs";
+
+export class IAMCreator {
+    public static createCodeBuildRole(self: Construct, roleName: string) {
+        const codeBuildRole = new Role(self, roleName, {
+            roleName: roleName,
+            assumedBy: new ServicePrincipal('codebuild.amazonaws.com'),
+            managedPolicies: [
+                {
+                    managedPolicyArn: 'arn:aws:iam::aws:policy/AdministratorAccess',
+                },
+            ],
+        });
+    }
+    public static createSNSPublishPolocyForCostServices(snsTopic: Topic): PolicyStatement {
+        const SNSPublishPolicy: PolicyStatement = new PolicyStatement({
+            sid: "SNSPublishingPermissions",
+            effect: Effect.ALLOW,
+            principals: [
+                new ServicePrincipal("costalerts.amazonaws.com"),
+                new ServicePrincipal("budgets.amazonaws.com"),
+            ],
+            actions: ["SNS:Publish"],
+            resources: [snsTopic.topicArn],
+        });
+        return SNSPublishPolicy;
+    }
+}
